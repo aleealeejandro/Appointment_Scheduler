f408a6d64d098c58d654338b7e978de2bd8708bb
alexanderpadilla <alexander.padilla@g.austincc.edu>
2022-07-05 18:53:16 -0500

    Cleaned up code

--- src/controller/MainUIController.java
+++ src/controller/MainUIController.java
@@ -1,20 +1,15 @@
 package controller;
 
-import com.sun.javafx.fxml.builder.JavaFXSceneBuilder;
 import databaseQueries.AppointmentsQuery;
 import databaseQueries.CountriesQuery;
 import databaseQueries.CustomersQuery;
 import databaseQueries.DivisionsQuery;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
